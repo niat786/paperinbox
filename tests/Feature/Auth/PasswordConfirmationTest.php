7cc422fce6dc2d297807206df763c81fb2610f67
niat786 <niat786@gmail.com>
2025-07-14 01:19:30 +0500

    Install Pest

--- tests/Feature/Auth/PasswordConfirmationTest.php
+++ tests/Feature/Auth/PasswordConfirmationTest.php
@@ -1,51 +1,39 @@
 <?php
 
-namespace Tests\Feature\Auth;
-
 use App\Livewire\Auth\ConfirmPassword;
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Livewire\Livewire;
-use Tests\TestCase;
 
-class PasswordConfirmationTest extends TestCase
-{
-    use RefreshDatabase;
+test('confirm password screen can be rendered', function () {
+    $user = User::factory()->create();
 
-    public function test_confirm_password_screen_can_be_rendered(): void
-    {
-        $user = User::factory()->create();
+    $response = $this->actingAs($user)->get('/confirm-password');
 
-        $response = $this->actingAs($user)->get('/confirm-password');
+    $response->assertStatus(200);
+});
 
-        $response->assertStatus(200);
-    }
+test('password can be confirmed', function () {
+    $user = User::factory()->create();
 
-    public function test_password_can_be_confirmed(): void
-    {
-        $user = User::factory()->create();
+    $this->actingAs($user);
 
-        $this->actingAs($user);
+    $response = Livewire::test(ConfirmPassword::class)
+        ->set('password', 'password')
+        ->call('confirmPassword');
 
-        $response = Livewire::test(ConfirmPassword::class)
-            ->set('password', 'password')
-            ->call('confirmPassword');
+    $response
+        ->assertHasNoErrors()
+        ->assertRedirect(route('dashboard', absolute: false));
+});
 
-        $response
-            ->assertHasNoErrors()
-            ->assertRedirect(route('dashboard', absolute: false));
-    }
+test('password is not confirmed with invalid password', function () {
+    $user = User::factory()->create();
 
-    public function test_password_is_not_confirmed_with_invalid_password(): void
-    {
-        $user = User::factory()->create();
+    $this->actingAs($user);
 
-        $this->actingAs($user);
+    $response = Livewire::test(ConfirmPassword::class)
+        ->set('password', 'wrong-password')
+        ->call('confirmPassword');
 
-        $response = Livewire::test(ConfirmPassword::class)
-            ->set('password', 'wrong-password')
-            ->call('confirmPassword');
-
-        $response->assertHasErrors(['password']);
-    }
-}
+    $response->assertHasErrors(['password']);
+});
